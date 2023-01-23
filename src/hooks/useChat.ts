5f1e87b51b5b406aa15336f5c254c9d3c1b49e1a
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2023-01-23 11:23:41 -0500

    chage port sockets

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -13,7 +13,7 @@ export const useChat = (): ChatHook => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const newSocket = io("ws://18.220.94.7:8000/");
+    const newSocket = io("ws://3.144.144.67:8000/");
 
     setSocket(newSocket);
   }, []);
